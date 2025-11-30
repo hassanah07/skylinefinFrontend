40a313097f1f56f292025a3721213fe816b49146
ANOWARUL HASSAN <anowarulah07@gmail.com>
2025-11-30 21:14:46 +0530

    x

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,20 @@ const nextConfig = {
         ),
         pathname: "/uploads/**",
       },
+      {
+        protocol: "http",
+        hostname: process.env.NEXT_PUBLIC_HOST?.replace("https://", "").replace(
+          "http://",
+          ""
+        ),
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "http",
+        hostname: "72.60.202.189",
+        port: "",
+        pathname: "/uploads/**",
+      },
     ],
   },
 };
