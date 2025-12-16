b08d6c9d57790697a6b48f6614abc6c764a16baf
ANOWARUL HASSAN <anowarulah07@gmail.com>
2025-12-16 07:01:26 +0530

    X

--- src/app/loggedInAdmin/customer/profile/[slag]/page.js
+++ src/app/loggedInAdmin/customer/profile/[slag]/page.js
@@ -201,7 +201,7 @@ export default function AdminDetailPage({ params }) {
             >
               🔙
             </span>
-            <h1 className="text-3xl font-bold mb-8 ml-16">Admin Details</h1>
+            <h1 className="text-3xl font-bold mb-8 ml-16">Customers Details</h1>
 
             {/* Photo Section */}
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8">
